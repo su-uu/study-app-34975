2eafd2ecdc0c4c131c303c9128dafe8b62fe81ac
su-uu <suzutsuba1312128@gmail.com>
2021-08-15 02:13:56 +0900

    questionテーブルへのカラムの追加

--- db/migrate/20210814165305_create_questions.rb
+++ db/migrate/20210814165305_create_questions.rb
@@ -1,7 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :title,    null: false
+      t.text :text,       null: false
+      t.string :answer,   null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_14_095259) do
+ActiveRecord::Schema.define(version: 2021_08_14_165305) do
+
+  create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "text", null: false
+    t.string "answer", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_08_14_095259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "questions", "users"
 end
