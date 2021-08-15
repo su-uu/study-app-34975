01770420408c861bbba9edd379e473d3b1ef6acd
su-uu <suzutsuba1312128@gmail.com>
2021-08-16 03:26:24 +0900

    edit、updateアクションとルーティングの設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,6 +22,12 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
 
   def question_params
