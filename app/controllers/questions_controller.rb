19cb41a1ef766c364f2f69b99243e01d7d2db40f
su-uu <suzutsuba1312128@gmail.com>
2021-08-15 23:30:17 +0900

    newアクションの定義とviewファイルの修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,15 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
   end
 
+  private
   def question_params
     params.require(:question).permit(:title, :text, :answer, :image).merge(user_id: current_user.id)
+  end
+
 end
