615582dbe55f8f6138097c37a13389a36628fa0d
su-uu <suzutsuba1312128@gmail.com>
2021-08-16 18:30:38 +0900

    コントローラーの編集(destroy)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only:[:show, :edit, :update]
+  before_action :set_question, only:[:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
 
@@ -35,6 +35,11 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
+    if @question.destroy
+      redirect_to root_path
+    else
+      redirect_to question_path
+    end
   end
 
   private
