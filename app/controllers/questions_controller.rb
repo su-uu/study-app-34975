62c95bfb3efa9d9cdd9fc32b0ed85b101386fad4
su-uu <suzutsuba1312128@gmail.com>
2021-08-16 18:32:26 +0900

    Rubocopによる自動修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :set_question, only:[:show, :edit, :update, :destroy]
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
