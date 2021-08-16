c0ac356cebec9dbca8322ad3d724989d8bc9319e
su-uu <suzutsuba1312128@gmail.com>
2021-08-17 00:06:58 +0900

    crateアクションの定義

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,5 +5,18 @@ class AnswersController < ApplicationController
   end
 
   def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:input_answer).merge(user_id: current_user.id, question_id: params[:question_id])
   end
 end
