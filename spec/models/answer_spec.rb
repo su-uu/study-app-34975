81af3113a905d080d0b987ebaf1af29df6f90bdc
su-uu <suzutsuba1312128@gmail.com>
2021-08-17 02:24:32 +0900

    テストコード実装の準備

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '問題解答' do
+    before do
+      @answer = FactoryBot.build(:answer)
+    end
+
+    context '問題の解答ができるとき' do
+      it '全ての値が正しく入力されていれば保存できること' do
+      end
+    end
+
+    context '問題の解答ができないとき' do
+      it 'input_answerが空では保存できないこと' do
+      end
+      it 'userが紐付いていないと保存できないこと' do
+      end
+      it 'questionが紐付いていないと保存できないこと' do
+      end
+    end
+  end
 end
